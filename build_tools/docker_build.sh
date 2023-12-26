4c32ad9e6325d1408c4c78c3396cc1bb524c45e8
Riccardo Casula <riccardocasula13@gmail.com>
2023-12-26 19:59:47 +0000

    Applied changes to build script

--- build_tools/docker_build.sh
+++ build_tools/docker_build.sh
@@ -3,8 +3,9 @@
 set -e
 
 custom_tag=${custom_tag:-$(git log -n1 --format="%h")}
+IMAGE_NAME="droid_builder:$custom_tag"
 
-docker build . -f build_tools/docker/Dockerfile -t droid_builder
-docker run --name android_apk -v ./:/build droid_builder /build.sh 
+docker build . -f build_tools/docker/Dockerfile -t $IMAGE_NAME
+docker run --name android_apk -v ./:/build $IMAGE_NAME /build.sh 
 docker cp android_apk:/build/android/app/build/outputs/bundle/release/release.aab ./release.aab
 docker rm android_apk
