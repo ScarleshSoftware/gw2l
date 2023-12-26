e802df6a6b3133502be72bb92dea450c48cad11f
Riccardo Casula <riccardocasula13@gmail.com>
2023-12-26 19:58:29 +0000

    Cleaned up dependency tree. Added a custom tag to docker

--- build_tools/docker_build.sh
+++ build_tools/docker_build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+custom_tag=${custom_tag:-$(git log -n1 --format="%h")}
+
 docker build . -f build_tools/docker/Dockerfile -t droid_builder
 docker run --name android_apk -v ./:/build droid_builder /build.sh 
 docker cp android_apk:/build/android/app/build/outputs/bundle/release/release.aab ./release.aab
