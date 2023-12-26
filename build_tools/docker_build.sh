dff02142dd1b9c2788905b85df08e559462a1989
Riccardo Casula <riccardocasula13@gmail.com>
2023-12-26 19:34:08 +0000

    Fixed the docker builder

--- build_tools/docker_build.sh
+++ build_tools/docker_build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build build_tools/docker -t droid_builder
+docker build . -f build_tools/docker/Dockerfile -t droid_builder
 docker run --name android_apk -v ./:/build droid_builder /build.sh 
-docker cp android_apk:/build/android/app/build/outputs/apk/release/* ./release.abb
+docker cp android_apk:/build/android/app/build/outputs/bundle/release/release.aab ./release.aab
 docker rm android_apk
